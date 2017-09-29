866c0edeafa71b2383cc1e6efc93b467eb453209
ngan <ngan@NGAN-PC>
2017-09-29 18:06:24 -0400

    Add project files.

---
+++ Comp229-Assignment1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp229_Assignment1.Startup))]
+namespace Comp229_Assignment1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
